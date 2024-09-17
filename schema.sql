5e4ec4344887c1ae44f3fa1f83ec0ccd316036d3
nbscarth <nbscarth@hotmail.com>
2024-09-17 07:54:53 -0400

    file cleanup

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS overdose_deaths;
+
+CREATE TABLE overdose_deaths (
+    id SERIAL PRIMARY KEY,
+	countyname VARCHAR(100) NOT NULL,
+    year INT NOT NULL,
+    deaths INT,
+    geometry JSONB  -- To store county boundaries or GeoJSON data
+);
+
+select * from overdose_deaths;
